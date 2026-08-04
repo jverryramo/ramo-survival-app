1bc7662f5df5bc76fde25802e7d7d8b1373391cd
jverryramo <jverry@ramo.eco>
2026-08-04 16:25:29 -0400

    fix: incrémenter buildNumber à 2 pour soumission TestFlight

--- app.config.ts
+++ app.config.ts
@@ -49,6 +49,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
+    buildNumber: "2",
     "infoPlist": {
         "ITSAppUsesNonExemptEncryption": false
       }
